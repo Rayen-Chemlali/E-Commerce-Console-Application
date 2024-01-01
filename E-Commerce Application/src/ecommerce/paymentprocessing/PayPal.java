b4410d218076af03b2d8f45b2d3a1d68ead26594
Rayen-Chemlali <155291708+Rayen-Chemlali@users.noreply.github.com>
2024-01-01 19:12:33 +0100

    Update PayPal.java

--- E-Commerce Application/src/ecommerce/paymentprocessing/PayPal.java
+++ E-Commerce Application/src/ecommerce/paymentprocessing/PayPal.java
@@ -9,11 +9,5 @@ public class PayPal implements PaymentStrategy{
         String payPalEmail = scanner.nextLine();
         System.out.println("Payment successful! Amount of " + amount + " DT has been successfully processed using PayPal account"+payPalEmail);
 
-
-
-
-
-
-
     }
 }
